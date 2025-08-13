5d427ea98e87c6b2355a15a21dd39d4fffd6120e
Dingzb <dingzibiao@gmail.com>
2025-08-13 10:52:26 +0800

    修复：修复输入路径包含多余路径分隔符时导致的输出路径错误

--- src/cracknuts_squirrel/preprocessing_basic.py
+++ src/cracknuts_squirrel/preprocessing_basic.py
@@ -1,16 +1,15 @@
 from cracknuts_squirrel.trace_io import DaskZarrIO
 import dask.array as da
-import os
+from pathlib import Path
 
 class PPBasic(DaskZarrIO):
     def __init__(self, input_path=None, output_path=None, tile='/0/0/', **kwargs):
         super().__init__(input_path=input_path, output_path=output_path, tile=tile, **kwargs)
         self.out_traces = da.zeros((self.num_traces, self.num_samples), chunks=self.t.chunks)
 
-    # @staticmethod    
     def auto_out_filename(self):
-        base_name, ext = os.path.splitext(self.input_path)
-        self.output_path = f"{base_name}_{self.__class__.__name__}{ext}"
+        p = Path(self.input_path)
+        self.output_path = f"{p.with_suffix('')}_{self.__class__.__name__}{p.suffix}"
 
 
 if __name__ == "__main__":
